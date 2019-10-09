b555eccad45762cba094e857f9e203bd99714f14
sash <sash@nsfocus.com>
2019-10-09 14:21:25 +0800

    项目init -vue create 初始化安装

---
+++ vue3-router-vuex/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- 路由导航 -->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/page1">page1</router-link>|
+      <router-link to="/page2">page2</router-link>
+    </div>
+    <!-- 路由出口 -->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
